03dc1bb0646e526c7d9788dd15eebea28b0c591b
sylony <sylonydh@gmail.com>
2023-11-02 15:50:43 -0700

    a4finish

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,16 +1,39 @@
-import Classes from "./Classes";
-import Styles from "./Styles";
+import Add from "./Add";
 import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
-import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+
 function Assignment3() {
   return (
     <div>
-      <h1>Assignment 3</h1> <ConditionalOutput /> <Styles />
-      <Classes />
+      <h2>Assignment 3</h2>
+      <TodoList />
+      <hr />
+      <ul className="list-group">
+        <TodoItem
+          todo={{
+            title: "Buy milk",
+            done: true,
+            status: "COMPLETE",
+          }}
+        />
+        <TodoItem
+          todo={{
+            title: "Pick up kids",
+            done: false,
+            status: "IN_PROGRESS",
+          }}
+        />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </ul>
+      <TodoItem />
       <PathParameters />
       <JavaScript />
     </div>
   );
 }
+
 export default Assignment3;
