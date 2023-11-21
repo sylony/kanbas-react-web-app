38a9b3966e5ccd170c0234c47e2c50143beef9ef
sylony <91612382+sylony@users.noreply.github.com>
2023-11-20 23:32:02 -0800

    Delete 2023/fall/webdev/kanbas-react-web-app-5 directory

--- 2023/fall/webdev/kanbas-react-web-app-5/src/Labs/a3/FilterFunction.js
+++
@@ -1,4 +0,0 @@
-let numberArray1 = [1, 2, 4, 5, 6];
-const numbersGreaterThan2 = numberArray1.filter((a) => a > 2);
-const evenNumbers = numberArray1.filter((a) => a % 2 === 0);
-const oddNumbers = numberArray1.filter((a) => a % 2 !== 0);
--- 2023/fall/webdev/kanbas-react-web-app-5/src/Labs/a3/WorkingWithFunctions.js
+++
@@ -1,4 +0,0 @@
-import ImpliedReturn from "./ImpliedReturn";
-import FunctionParenthesisAndParametersFunctions from "./FunctionParenthesisAndParameters";
-ES5Functions;
-ArrowFunctions.js
